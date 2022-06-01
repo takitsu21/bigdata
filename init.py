4e1652e5d1f6f8294b5811f1a1faf88dcce424c0
QuentinBeauchet <quentinbeauchet@free.fr>
2022-06-01 23:51:09 +0200

    .

--- init.py
+++ init.py
@@ -4,7 +4,6 @@ import csv
 from dateutil.parser import parse
 import pandas as pd
 import sys
-from src import multi_thread
 
 db = redis.Redis(host='localhost', port=6379, db=0)
 
@@ -14,7 +13,7 @@ if start == 0:
     db.flushdb()
     db.flushall()
 
-def Feedback():
+if start < 1:
     print("1.Feedback...")
     with open("./DATA/Feedback/Feedback.csv", "r") as f:
         reader = csv.reader(f, quotechar='"', delimiter='|',
@@ -23,7 +22,7 @@ def Feedback():
             # asin | PersonId | feedback
             db.hset(row[0], row[1], row[2])
 
-def BrandByProduct():
+if start < 2:
     print("2.BrandByProduct...")
     with open("./DATA/Product/BrandByProduct.csv", "r") as f:
         reader = csv.reader(f, quotechar='"', delimiter=',',
@@ -32,7 +31,7 @@ def BrandByProduct():
             # brand | asin
             db.hset(row[1], "brand", row[0])
 
-def Product():
+if start < 3:
     print("3.Product...")
     with open("./DATA/Product/Product.csv", "r") as f:
         next(f)
@@ -44,7 +43,7 @@ def Product():
             db.hset(row[0], "price", float(row[2]))
             db.hset(row[0], "imgUrl", row[3])
 
-def Customer():
+if start < 4:
     print("4.Customer...")
     with open("./DATA/Customer/person_0_0.csv", "r") as f:
         next(f)
@@ -61,7 +60,7 @@ def Customer():
             db.hset(row[0], "browserUsed", row[7])
             db.hset(row[0], "place", float(row[8]))
 
-def Vendor():
+if start < 5:
     print("5.Vendor...")
     with open("./DATA/Vendor/Vendor.csv", "r") as f:
         next(f)
@@ -72,7 +71,7 @@ def Vendor():
             db.hset(row[0], "Country", row[1])
             db.hset(row[0], "Industry", row[2])
 
-def Order():
+if start < 6:
     print("6.Order...")
     df = pd.read_json('./DATA/Order/Order.json', lines=True)
     for index, row in df.iterrows():
@@ -90,7 +89,7 @@ def Order():
         db.rpush(f"{OrderId}_Orderline", *asins)
 
 
-def Invoice():
+if start < 7:
     print("7.Invoice...")
     tree = ET.parse(open("./DATA/Invoice/Invoice.xml", "r"))
     for invoice in tree.findall('Invoice.xml'):
@@ -106,7 +105,7 @@ def Invoice():
             asins.append(Orderline.find("asin").text)
         db.rpush(f"{OrderId}_Orderline", *asins)
 
-def person_hasInterest_tag_0_0():
+if start < 8:
     print("8.person_hasInterest_tag_0_0.csv...")
     with open("./DATA/SocialNetwork/person_hasInterest_tag_0_0.csv", "r") as f:
         next(f)
@@ -116,7 +115,7 @@ def person_hasInterest_tag_0_0():
             # Person.id | Tag.id
             db.rpush(f"{row[0]}_Tags", row[1])
 
-def person_knows_person_0_0():
+if start < 9:
     print("9.person_knows_person_0_0...")
     with open("./DATA/SocialNetwork/person_knows_person_0_0.csv", "r") as f:
         next(f)
@@ -126,13 +125,10 @@ def person_knows_person_0_0():
             # Person.id | Person.id | creationDate
             db.rpush(f"{row[0]}_Knows", row[1])
 
-def post_0_0():
+if start < 10:
     print("10.post_0_0...")
-    with open("./DATA/SocialNetwork/post_0_0.csv", "r") as f:
-        next(f)
-        reader = csv.reader(f, quotechar='"', delimiter='|',
-                            quoting=csv.QUOTE_ALL, skipinitialspace=True)
-        for row in reader:
+    for chunk in pd.read_csv("./DATA/SocialNetwork/post_0_0.csv", sep="|", chunksize=20000):
+        for index, row in chunk.iterrows():
             # id | imageFile | creationDate | locationIP | browserUsed | language | content | length
             db.hset(row[0], "imageFile", row[1])
             db.hset(row[0], "creationDate", parse(row[2]).timestamp())
@@ -142,7 +138,8 @@ def post_0_0():
             db.hset(row[0], "content", row[6])
             db.hset(row[0], "length", float(row[7]))
 
-def post_hasCreator_person_0_0():
+
+if start < 11:
     print("11.post_hasCreator_person_0_0...")
     with open("./DATA/SocialNetwork/post_hasCreator_person_0_0.csv", "r") as f:
         next(f)
@@ -152,7 +149,7 @@ def post_hasCreator_person_0_0():
             # Post.id | Person.id
             db.rpush(f"{row[1]}_Posts", row[0])
 
-def post_hasTag_tag_0_0():
+if start < 12:
     print("12.post_hasTag_tag_0_0...")
     with open("./DATA/SocialNetwork/post_hasTag_tag_0_0.csv", "r") as f:
         next(f)
@@ -161,21 +158,3 @@ def post_hasTag_tag_0_0():
         for row in reader:
             # Post.id | Tag.id
             db.rpush(f"{row[0]}_Tags", row[1])
-
-
-fs = [
-    lambda : Feedback(),
-    lambda : BrandByProduct(),
-    lambda : Product(),
-    lambda : Customer(),
-    lambda : Vendor(),
-    lambda : Order(),
-    lambda : Invoice(),
-    lambda : person_hasInterest_tag_0_0(),
-    lambda : person_knows_person_0_0(),
-    lambda : post_0_0(),
-    lambda : post_hasCreator_person_0_0(),
-    lambda : post_hasTag_tag_0_0()
-][start:]
-
-multi_thread.multi_thread(fs)
